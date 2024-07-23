d51e43ca1d94e9c06281071d81da2a928401b35d
Ratna Katuwal <115388990+Ratna-katuwal1@users.noreply.github.com>
2024-07-23 15:19:13 +0545

    final one

--- app/src/main/java/com/ratna/memegenerator/MainActivity.java
+++ app/src/main/java/com/ratna/memegenerator/MainActivity.java
@@ -1,6 +1,8 @@
 package com.ratna.memegenerator;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,61 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView memeOne, memeTwo, memeThree, memeFour, memeFive, memeSix, memeSeven, memeEight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        memeOne = findViewById(R.id.memeOne);
+        memeTwo = findViewById(R.id.memeTwo);
+        memeThree = findViewById(R.id.memeThree);
+        memeFour = findViewById(R.id.memeFour);
+        memeFive = findViewById(R.id.memeFive);
+        memeSix = findViewById(R.id.memeSix);
+        memeSeven = findViewById(R.id.memeSeven);
+        memeEight = findViewById(R.id.memeEight);
+
+        memeOne.setOnClickListener(view -> {
+            gotoGenerateMemeScreen(1);
         });
+
+        memeTwo.setOnClickListener(view -> {
+            gotoGenerateMemeScreen(2);
+        });
+
+        memeThree.setOnClickListener(view -> {
+            gotoGenerateMemeScreen(3);
+        });
+
+        memeFour.setOnClickListener(view -> {
+            gotoGenerateMemeScreen(4);
+        });
+
+        memeFive.setOnClickListener(view -> {
+            gotoGenerateMemeScreen(5);
+        });
+
+        memeSix.setOnClickListener(view -> {
+            gotoGenerateMemeScreen(6);
+        });
+
+        memeSeven.setOnClickListener(view -> {
+            gotoGenerateMemeScreen(7);
+        });
+
+        memeEight.setOnClickListener(view -> {
+            gotoGenerateMemeScreen(8);
+        });
+
+
+    }
+
+    private void gotoGenerateMemeScreen(int name) {
+        Intent intent = new Intent(this, MemeGenerator.class);
+        intent.putExtra("drawableName", "meme" + name);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/ratna/memegenerator/MemeGenerator.java
+++ app/src/main/java/com/ratna/memegenerator/MemeGenerator.java
@@ -1,24 +1,144 @@
 package com.ratna.memegenerator;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class MemeGenerator extends AppCompatActivity {
+    private static final int PERMISSION_REQUEST_CODE = 100;
+    TextView topTextView, bottomTextView;
+    ImageView memeImageView;
+    Button generateButton;
+    LinearLayout memeLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_meme_generator);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+       topTextView = findViewById(R.id.topTextView);
+       bottomTextView = findViewById(R.id.bottomTextView);
+       memeImageView = findViewById(R.id.memeImageView);
+       generateButton = findViewById(R.id.generateMemeBtn);
+       memeLayout = findViewById(R.id.memeLayout);
+
+       String drawableName = getIntent().getStringExtra("drawableName");
+       int resourceID = getResources().getIdentifier(drawableName, "drawable", getPackageName());
+       memeImageView.setImageResource(resourceID);
+       generateButton.setOnClickListener(view -> {
+           generateMeme();
+       });
+    }
+
+    private void generateMeme() {
+        if (checkPermission()){
+            saveLayoutToGallery();
+        } else {
+            requestPermission();
+        }
+    }
+    private boolean checkPermission() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q){
+            int result = ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+            return result == PackageManager.PERMISSION_GRANTED;
+        } else {
+            return true;
+        }
+    }
+
+    private void requestPermission() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResutls) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResutls);
+        if (requestCode == PERMISSION_REQUEST_CODE){
+            if (grantResutls.length > 0 && grantResutls[0] == PackageManager.PERMISSION_GRANTED){
+                saveLayoutToGallery();
+            } else {
+                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void saveLayoutToGallery() {
+        Bitmap bitmap = getBitmapFormView(memeLayout);
+        saveBitmapToGallery(bitmap, this);
+    }
+
+    private Bitmap getBitmapFormView(LinearLayout memeLayout) {
+        Bitmap bitmap = Bitmap.createBitmap(memeLayout.getWidth(), memeLayout.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        Drawable bgDrawable = memeLayout.getBackground();
+        if (bgDrawable != null){
+            bgDrawable.draw(canvas);
+        } else {
+            canvas.drawColor(Color.WHITE);
+        }
+        memeLayout.draw(canvas);
+        return bitmap;
+    }
+
+    private void saveBitmapToGallery(Bitmap bitmap, MemeGenerator memeGenerator) {
+        String saveImagePath = null;
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + ".jpg";
+        File storageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES) + "/memeGenerator");
+        if (!storageDir.exists()) {
+            storageDir.mkdirs();
+        }
+        File imageFile = new File(storageDir, imageFileName);
+        saveImagePath = imageFile.getAbsolutePath();
+        Context context = null;
+        try {
+            OutputStream fOut = new FileOutputStream(imageFile);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fOut);
+            fOut.close();
+            MediaScannerConnection.scanFile(null, new String[]{imageFile.toString()}, null, new MediaScannerConnection.OnScanCompletedListener() {
+                @Override
+                public void onScanCompleted(String path, Uri uri) {
+                    Log.i("ExternalStorage", "Scanned " + path + ":");
+                    Log.i("ExternalStorage", "-> uri=" + uri);
+                }
+            });
+            Toast.makeText(this, "Image saved to gallery", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(null, "Failed to save image!", Toast.LENGTH_SHORT).show();
+        }
     }
 }
